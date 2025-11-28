8e3e202996a3de60dff23eb18d6abd20d6f06537
ToastnotAI <ryanchristie03@gmail.com>
2025-11-28 11:26:27 +0000

    added a draw_dashed method and modified modified config variables Added a circle class

--- shape-renderer.py
+++ shape-renderer.py
@@ -1,12 +1,21 @@
 import turtle
 t = turtle.Turtle()
 
+
 #Shape is an abstract class
 class Shape():
-    # scale will change size of all shapes
-    scale = 1
+    shapeWidth = 50
+    dashLength = 5
     modifiers = []
+
+    def __init__(self, modifiers = None):
+        if modifiers is not None:
+            self.modifiers = modifiers
+
     def draw(self, t):
+        raise NotImplementedError("Subclasses should implement this method")
+
+    def draw_dashed(self, t):
         raise NotImplementedError("Subclasses should implement this method")
 
     def fill(self, t):
@@ -15,22 +24,74 @@ class Shape():
         t.end_fill()
     
     def begin(self, t):
+        if "dashed" in self.modifiers:
+            self.draw_dashed(t)
         if "fill" in self.modifiers:
             self.fill(t)
-        if self.modifiers == []:
+        if "fill" not in self.modifiers and "dashed" not in self.modifiers:
             self.draw(t)
 
 
 
 class Square(Shape):
-    def __init__(self, modifiers = None):
-        if modifiers is not None:
-            self.modifiers = modifiers
 
     def draw(self, t):
         for side in range(4):
-            t.forward(100 * Shape.scale)
+            t.forward(shape.shapeWidth)
             t.right(90)
+    
+    def draw_dashed(self, t):
+        done = False
+        progress = [0, 0] # [distance covered on current side, sides completed]
+        penDown = True
+        while not done:
+            if progress[1] == 3 and progress[0] >= shape.shapeWidth:
+                done = True
+            elif progress[0] >= shape.shapeWidth:
+                t.right(90)
+                progress[0] = 0
+                progress[1] += 1
+            elif penDown:
+                t.forward(shape.dashLength)
+                progress[0] += shape.dashLength
+                penDown = False
+                t.penup()
+            else:
+                t.forward(shape.dashLength)
+                progress[0] += shape.dashLength
+                penDown = True
+                t.pendown()
+        t.right(90)
+        t.penup()
+
+class Circle(Shape):
+    turns = 72
+    
+    def draw(self, t, dashed = False):
+        pen = t.isdown()
+        t.penup()
+        t.forward(Shape.shapeWidth / 2)
+        if pen:
+            t.pendown()
+        for turn in range(self.turns):
+            t.right(360 / self.turns)
+            t.forward((3.14 * Shape.shapeWidth) / self.turns) # use shapeWidth as diameter to find circumference
+            if dashed: # dashLength is not compatible with circles so it is handled differently
+                if t.isdown():
+                    t.penup()
+                else:
+                    t.pendown()
+
+        t.penup()
+        t.backward(Shape.shapeWidth / 2)
+        if pen:
+            t.pendown()
+    
+
+    def draw_dashed(self, t):
+        self.draw(t, dashed = True)
+
+
 
 
 
@@ -41,6 +102,8 @@ class Factory():
     def create_shape(shapeType):
         if shapeType[0] == "square":
             return Square(shapeType[1:])
+        elif shapeType[0] == "circle":
+            return Circle(shapeType[1:])
         else:
             raise ValueError("Unknown shape type: {}, Ensure factory was fed a list".format(shapeType[0]))
 
@@ -48,12 +111,16 @@ if __name__ == "__main__":
     shapes = []
     shapes.append(Factory.create_shape(["square", "fill"]))
     shapes.append(Factory.create_shape(["square"]))
+    shapes.append(Factory.create_shape(["square", "dashed"]))
+    shapes.append(Factory.create_shape(["square", "fill", "dashed"]))
+    shapes.append(Factory.create_shape(["circle", "dashed"]))
+    shapes.append(Factory.create_shape(["circle", "fill"]))
     t.penup()
     t.goto(-200, 200)
     t.pendown()
     for shape in shapes:
         shape.begin(t)
         t.penup()
-        t.forward(150 * Shape.scale)
+        t.forward(shape.shapeWidth + (0.1 * shape.shapeWidth))
         t.pendown()
     turtle.done()
